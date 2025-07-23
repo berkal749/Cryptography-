0d17b181d5b9df7e5d140f27043993bf8ef00e8e
berkal749 <berkalachrefpro@gmail.com>
2025-07-23 15:21:31 +0100

    1.1

--- script.js
+++ script.js
@@ -1,57 +1,44 @@
-
-
 var coin1 = document.getElementById("right");
-
-
-
-async function getData( ) {
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1';
+async function getData() {
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1";
   try {
-     const response = await fetch(url, {
+    const response = await fetch(url, {
       method: "GET",
       headers: {
         Accept: "application/json",
-        
       },
     });
 
-    
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
     const json = await response.json();
 
-    
-    for(let coins of json){
-        console.log(json);
-        
-        
-    var tick = document.createElement("div");
-    tick.setAttribute("id",json.id);
-    tick.setAttribute("class","tick");
+    for (let coins of json) {
+      console.log(json);
 
-    var name = document.createElement(  "div");
-    
-    name.innerText= coins.name;
-    tick.appendChild(name);
+      var tick = document.createElement("div");
+      tick.setAttribute("id", json.id);
+      tick.setAttribute("class", "tick");
 
-    var price = document.createElement(  "p");
-    price.innerText= `currently:${coins.current_price}$`;
-    
-    
-    tick.appendChild(price);
+      var name = document.createElement("div");
 
-    var current = document.createElement("div");
-    current.innerText= `${coins.price_change_24h}%`;
+      name.innerText = coins.name;
+      tick.appendChild(name);
 
-    tick.appendChild(current);
-    coin1.appendChild(tick)
+      var price = document.createElement("p");
+      price.innerText = `currently:${coins.current_price}$`;
 
-}
+      tick.appendChild(price);
 
+      var current = document.createElement("div");
+      current.innerText = `${coins.price_change_24h}%`;
 
-
+      tick.appendChild(current);
+      coin1.appendChild(tick);
+    }
   } catch (error) {
     console.error(error.message);
   }
